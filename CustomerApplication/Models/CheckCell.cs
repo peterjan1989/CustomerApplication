5f6937ed5f0aa5e0cf91533fefbf7197fe8e2f09
Roson <peterjan2004@yahoo.com.tw>
2015-07-06 14:55:46 +0800

    Add to git

---
+++ CustomerApplication/Models/CheckCell.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CustomerApplication.Models
+{
+    public class CheckCellAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid
+    (object value, ValidationContext validationContext)
+        {
+            Regex rx = new Regex(@"\d{4}-\d{6}");
+
+            string cell = (string)value == null ? "" : (string)value;
+
+            if (!rx.IsMatch(cell))
+                return new ValidationResult("手機 格試錯誤");
+
+            return ValidationResult.Success;
+        }
+    }
+}
